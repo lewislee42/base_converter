7810ef65016ac17cda06f21e7b98898cc5ed16d8
Lewis Lee <133765185+lewislee42@users.noreply.github.com>
2023-09-05 13:16:38 +0000

    initial commit

---
+++ main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/05 13:11:35 by codespace         #+#    #+#             */
+/*   Updated: 2023/09/05 13:16:11 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <tsdio.h>
+
+// -a for ascii
+// -o for octal
+// -b for binary
+// -h for hexa
+// -c for character
+
+// can combine -c with others
+// -ch will convert from character to hexa
+// -cb will convert from character to binary
+// etc..
+
+int main(int ac, char **av)
+{
+    
+}
